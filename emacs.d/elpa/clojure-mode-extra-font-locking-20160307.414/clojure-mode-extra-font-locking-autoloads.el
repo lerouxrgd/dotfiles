8cffb8fbc8cbe91efc613ac93e7f0945741b54c0
lerouxrgd <leroux.rgd@gmail.com>
2016-04-19 00:33:06 +0900

    removing elpa data

--- emacs.d/elpa/clojure-mode-extra-font-locking-20160307.414/clojure-mode-extra-font-locking-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; clojure-mode-extra-font-locking-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/clojure-mode-extra-font-locking-20160307.414/clojure-mode-extra-font-locking-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/clojure-mode-extra-font-locking-20160307.414/clojure-mode-extra-font-locking.el")
-;;;;;;  (22292 64414 717065 709000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; clojure-mode-extra-font-locking-autoloads.el ends here
