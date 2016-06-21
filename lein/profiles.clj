03d10351c3f99ef11e19cdad909de0047134952a
lerouxrgd <leroux.rgd@gmail.com>
2016-06-21 21:11:24 +0900

    cleanup lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,2 @@
-{:user {:plugins [[cider/cider-nrepl "0.12.0"]
-                  [lein-try "0.4.3"]
-                  [lein-ancient "0.6.10"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+{:user {:plugins [[lein-try "0.4.3"]
+                  [lein-ancient "0.6.10"]]}}
