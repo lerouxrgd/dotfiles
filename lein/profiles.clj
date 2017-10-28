8eb69a8b1a4eaeb2a0d43188e2541225641ff2c1
lerouxrgd <leroux.rgd@gmail.com>
2017-10-28 19:14:02 +0200

    update lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-try "0.4.3"]
-                  [lein-ancient "0.6.10"]
-                  [lein-exec "0.3.6"]]}}
+                  [lein-ancient "0.6.14"]
+                  [lein-exec "0.3.7"]]}}
