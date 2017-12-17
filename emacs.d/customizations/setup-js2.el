e85ac8a2b9f4088c71387fcfc31317b012fbad54
Romain Leroux <raru@N85-N870HL.numericable.fr>
2017-12-17 17:24:03 +0100

    support for js (nav, autocomplete)

---
+++ emacs.d/customizations/setup-js2.el
@@ -0,0 +1,57 @@
+;;;;
+;; Javascript
+;;;;
+
+(require 'js2-mode)
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(setq js-indent-level 2)
+
+;; Better imenu
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+
+(require 'js2-refactor)
+(require 'xref-js2)
+
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+;; js-mode (which js2 is based on) binds "M-." which conflicts with xref,
+;; so unbind it.
+(define-key js-mode-map (kbd "M-.") nil)
+
+(add-hook 'js2-mode-hook (lambda ()
+  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
+(require 'company)
+(require 'company-tern)
+
+(add-to-list 'company-backends 'company-tern)
+(add-hook 'js2-mode-hook (lambda ()
+                           (tern-mode)
+                           (company-mode)))
+                           
+;; Disable completion keybindings, as we use xref-js2 instead
+(define-key tern-mode-keymap (kbd "M-.") nil)
+(define-key tern-mode-keymap (kbd "M-,") nil)
+
+;;;;
+;; HTML
+;;;;
+
+(add-hook 'html-mode-hook 'subword-mode)
+(eval-after-load "sgml-mode"
+  '(progn
+     (require 'tagedit)
+     (tagedit-add-paredit-like-keybindings)
+     (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
+
+;;;;
+;; JSON
+;;;;
+
+(require 'flymake-json)
+
+(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
+(add-hook 'json-mode-hook 'flymake-json-load)
