215e5db1dadf01d317dbd863f0f349c4b490df8f
lerouxrgd <leroux.rgd@gmail.com>
2018-06-08 01:13:14 +0200

    adding rust

---
+++ emacs.d/customizations/setup-rust.el
@@ -0,0 +1,10 @@
+;; Rust
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+
+(add-hook 'racer-mode-hook #'company-mode)
+
+(require 'rust-mode)
+(define-key rust-mode-map (kbd "C-c TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
