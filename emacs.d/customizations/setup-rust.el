84a2498cc4be8e2bc6d86c3f3ce3588a317081da
Leroux Romain <leroux.rgd@gmail.com>
2018-08-01 23:11:54 +0200

    a bit more rust

--- emacs.d/customizations/setup-rust.el
+++ emacs.d/customizations/setup-rust.el
@@ -9,6 +9,7 @@
 (setq company-tooltip-align-annotations t)
 (setq rust-format-on-save t)
 
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
 (add-hook 'rust-mode-hook #'racer-mode)
 
 (add-hook 'racer-mode-hook #'eldoc-mode)
