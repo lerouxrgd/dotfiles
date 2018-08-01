f70eed3fca21d4d60045a34808ba2b652062af2d
Leroux Romain <leroux.rgd@gmail.com>
2018-08-01 22:48:09 +0200

    cleanup and refactoring

--- emacs.d/customizations/setup-go.el
+++ emacs.d/customizations/setup-go.el
@@ -2,17 +2,16 @@
 ;; Golang
 ;;;;
 
-(add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake"))
-(require 'go-flymake)
-;;(require 'go-flycheck)
+(require 'go-guru)
 
 (defun my-go-mode-hook ()
-  (add-hook 'before-save-hook 'gofmt-before-save) ; gofmt before every save
-  (setq gofmt-command "goimports")                ; gofmt uses invokes goimports
-  (go-guru-hl-identifier-mode)                    ; highlight identifiers
-  (local-set-key (kbd "M-.") 'godef-jump)         ; Go to definition
-  (local-set-key (kbd "M-*") 'pop-tag-mark)       ; Return from whence you came
+  (add-hook 'before-save-hook 'gofmt-before-save) ;; gofmt before every save
+  (setq gofmt-command "goimports")                ;; gofmt uses invokes goimports
+  (go-guru-hl-identifier-mode)                    ;; highlight identifiers
+  (local-set-key (kbd "M-.") 'godef-jump)         ;; Go to definition
+  (local-set-key (kbd "M-,") 'pop-tag-mark)       ;; Return from whence you came
   )
+
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (with-eval-after-load 'go-mode
@@ -20,4 +19,3 @@
   (require 'auto-complete-config)
   (ac-config-default))
 
-(require 'go-guru)
